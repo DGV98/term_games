269b7935ec711a6d159fc01dae9753318e37cbf4
David Gonzalez-Velez <davidgonzalez1019@gmail.com>
2025-10-17 19:01:06 +0000

    Added snake and utility

--- snake.py
+++ snake.py
@@ -1,2 +1,177 @@
-def play_snake(stdscr, win1, win2):
-    pass
+import curses
+from random import randint
+
+
+class Node:
+    """
+    This is the utility class for a Linked List, will be used to create a snake
+    """
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.next = None
+
+
+class Snake:
+    """
+    Snake class, essentially a pointer to the head and tail of the linked list
+    """
+
+    def __init__(self, head: Node):
+        self.head = head
+        self.tail = head
+        self.length = 1
+
+    def grow(self, node: Node):
+        """
+        Attaches a node to the end of the linked list and updates the length
+
+        Args:
+            node (Node): The node to add to the linked list
+        """
+        self.tail.next = node
+        self.tail = self.tail.next
+        self.length += 1
+
+
+class GridState:
+    """
+    Class to keep track of the grid state, the snake, and updating everything regarding the game.
+    """
+
+    def __init__(self, max_y: int, max_x: int):
+        self.width = max_x - 2
+        self.height = max_y - 1
+        head_x, head_y = self.width // 2, self.height // 2
+        self.grid = [[" "] * self.width for i in range(self.height)]
+        head = Node(head_x, head_y)
+        self.head = "^"
+        self.snake = Snake(head)
+        self.food_x = 0
+        self.food_y = 0
+        self.generate_food()
+        self.update_grid()
+        self.exit_flag = False
+
+    def game_cases(self, key):
+        """
+        The game cases, either collision or growth
+
+        Args:
+            key (string): the direction to move the head of the snake
+        """
+        self.update_snake_head(key)
+        if self.snake.head.x == self.width or self.snake.head.x < 0:
+            self.exit_flag = True
+            return
+        elif self.snake.head.y == self.height or self.snake.head.y < 0:
+            self.exit_flag = True
+            return
+        elif self.grid[self.snake.head.y][self.snake.head.x] == "#":
+            self.exit_flag = True
+            return
+        elif self.snake.head.x == self.food_x and self.snake.head.y == self.food_y:
+            new_node = Node(self.snake.tail.x, self.snake.tail.y)
+            self.snake.grow(new_node)
+            self.generate_food()
+        self.update_grid()
+
+    def update_grid(self):
+        """
+        Updates the grid with the relevant information stored in the linked list and food object
+        """
+        self.grid[self.food_y][self.food_x] = "@"
+        curr = self.snake.head.next
+        while curr:
+            self.grid[curr.y][curr.x] = "#"
+            curr = curr.next
+        self.grid[self.snake.head.y][self.snake.head.x] = self.head
+
+    def update_snake_head(self, key):
+        """
+        Updates the head of the snake based on the direction, and updates the rest of the snake using update_snake
+
+        Args:
+            key (str): The direction from the key presses
+        """
+        prev_x, prev_y = self.snake.head.x, self.snake.head.y
+        if key == curses.KEY_UP:  # Up arrow
+            self.snake.head.y -= 1
+            self.head = "^"
+        elif key == curses.KEY_DOWN:  # Down arrow
+            self.snake.head.y += 1
+            self.head = "v"
+        elif key == curses.KEY_RIGHT:  # Right arrow
+            self.snake.head.x += 1
+            self.head = ">"
+        elif key == curses.KEY_LEFT:  # Left arrow
+            self.snake.head.x -= 1
+            self.head = "<"
+        self.update_snake(prev_x, prev_y, self.snake.head.next)
+
+    def update_snake(self, prev_x, prev_y, node: Node):
+        """
+        Recursive function to update the other nodes of the snake based on the direction of the head
+
+        Args:
+            prev_x (int): previous x position
+            prev_y (int): previous y position
+            node (Node): the node to update
+        """
+        if not node:
+            self.grid[prev_y][prev_x] = " "
+            return
+        tmp_x, tmp_y = node.x, node.y
+        node.x, node.y = prev_x, prev_y
+        self.update_snake(tmp_x, tmp_y, node.next)
+
+    def generate_food(self):
+        """
+        Update the food position in the grid
+        """
+        x = randint(0, self.width - 1)
+        y = randint(0, self.height - 1)
+        while self.grid[y][x] == "#" or (x, y) == (
+            self.snake.head.x,
+            self.snake.head.y,
+        ):
+            x = randint(0, self.width - 1)
+            y = randint(0, self.height - 1)
+        self.food_x = x
+        self.food_y = y
+
+
+def paint_screen(win: curses.window, grid: GridState):
+    win.box()
+    for i, line in enumerate(grid.grid):
+        win.addstr(i + 1, 1, "".join(line))
+    win.refresh()
+
+
+def play_snake(stdscr: curses.window, win1: curses.window, win2: curses.window):
+    """
+    Play the game, main game loop, will be threaded along with the key strokes
+    """
+    max_y, max_x = win2.getmaxyx()
+    grid = GridState(max_y - 1, max_x)
+    paint_screen(win2, grid)
+    while True:
+        score = f"SCORE: {grid.snake.length}"
+        x = (max_x - len(score)) // 2
+        win1.addstr(6, x, score)
+        win1.refresh()
+        key = stdscr.getch()
+        if key == ord("q"):
+            return grid.snake.length
+        else:
+            if key in [
+                curses.KEY_UP,
+                curses.KEY_DOWN,
+                curses.KEY_RIGHT,
+                curses.KEY_LEFT,
+            ]:
+                grid.game_cases(key)
+                if grid.exit_flag:
+                    return grid.snake.length
+            paint_screen(win2, grid)
--- utilities.py
+++ utilities.py
@@ -1,11 +1,169 @@
-def get_scores(scoreboard_path: str):
-    pass
-
-
-def update_scoreboard(scoreboard_path: str):
-    pass
+import curses
+import json
 
 
 class ScreenWindow:
-    def __init__(self):
-        pass
+    """
+    The base window class for our use case
+    """
+
+    def __init__(
+        self,
+        stdscr: curses.window,
+        mode: str,
+        lines: list[str],
+        header: str = "",
+        input_line_idx: int = 0,
+        padding: int = 2,
+    ):
+        self.stdscr = stdscr
+        self.mode = mode
+        self.header = header
+        self.lines = lines
+        self.input_line_idx = input_line_idx
+        self.padding = padding
+        self.selected_idx = 0  # for menu
+        self.input_result = ""  # for input
+
+        content_lines = [header] if header else []
+        content_lines += lines
+        self.width = max(len(line) for line in content_lines) + 2 * padding
+        if self.mode == "input":
+            self.width += 3
+        self.height = len(lines) + 2 * padding + (2 if header else 0)
+
+        max_y, max_x = stdscr.getmaxyx()
+        y = (max_y - self.height) // 2
+        x = (max_x - self.width) // 2
+        self.win = curses.newwin(self.height, self.width, y, x)
+        self.win.keypad(True)
+
+    def get_win(self):
+        """
+        Window getting funtion to manipulate window object
+
+        Returns:
+            curses.window: the window object
+        """
+        return self.win
+
+    def mv_win(self, y, x):
+        self.win.mvwin(y, x)
+
+    def draw(self):
+        """
+        Drawing all the content on the window
+        """
+        self.win.clear()
+        self.win.box()
+        y = self.padding
+
+        if self.header:
+            x = (self.width - len(self.header)) // 2
+            self.win.addstr(y, x, self.header, curses.A_BOLD)
+            y += 1
+            self.win.hline(
+                y, self.padding, curses.ACS_HLINE, self.width - 2 * self.padding
+            )
+            y += 1
+
+        for idx, line in enumerate(self.lines):
+            display = line
+            attr = curses.A_NORMAL
+
+            if self.mode == "menu" and idx == self.selected_idx:
+                attr = curses.A_REVERSE
+
+            x = (self.width - len(display)) // 2
+            self.win.addstr(y + idx, x, display, attr)
+
+        self.win.refresh()
+
+    def run(self):
+        """
+        Runs the drawing function
+
+        Returns:
+            str or None: Depending on the type of screen, returns a string or nothing
+        """
+        self.stdscr.clear()
+        self.stdscr.refresh()
+        if self.mode == "static":
+            self.draw()
+            self.stdscr.getch()
+
+        elif self.mode == "input":
+            while True:
+                self.draw()
+                y = self.padding + (2 if self.header else 0) + self.input_line_idx
+                x = self.padding + len(self.lines[self.input_line_idx]) - 2
+                curses.echo()
+                curses.curs_set(2)
+                self.win.move(y, x)
+                self.input_result = self.win.getstr(y, x, 30).decode("utf-8")
+                curses.curs_set(0)
+                curses.noecho()
+                if len(self.input_result) < 4 and self.input_result:
+                    break
+            return self.input_result
+
+        elif self.mode == "menu":
+            while True:
+                self.draw()
+                key = self.stdscr.getch()
+
+                if key in [curses.KEY_UP, ord("k")]:
+                    self.selected_idx = (self.selected_idx - 1) % len(self.lines)
+                elif key in [curses.KEY_DOWN, ord("j")]:
+                    self.selected_idx = (self.selected_idx + 1) % len(self.lines)
+                elif key in [curses.KEY_ENTER, 10, 13]:
+                    return self.lines[self.selected_idx]
+
+
+def get_scores(scoreboard_path):
+    with open(scoreboard_path, "r") as scoreboard:
+        scores = json.load(scoreboard)
+    return scores
+
+
+def update_scoreboard(
+    player: str,
+    score: int,
+    game: str,
+    scores: dict,
+    difficulty: str,
+    scoreboard_path: str,
+):
+    """
+    Update the json holding all the scores
+
+    Args:
+        player (str): The player
+        score (int): The score
+        game (str): The game being updated
+    """
+    player = player.upper()
+    game = game.lower()
+    difficulty = difficulty.lower()
+    if game not in scores:
+        if not difficulty:
+            scores[game] = {player: score}
+        else:
+            scores[game] = {difficulty: {player: score}}
+    else:
+        if not difficulty:
+            if player not in scores[game]:
+                scores[game][player] = score
+            else:
+                scores[game][player] = max(score, scores[game][player])
+        else:
+            if difficulty not in scores[game]:
+                scores[game][difficulty] = {player: score}
+            else:
+                if player not in scores[game]:
+                    scores[game][difficulty][player] = score
+                else:
+                    scores[game][difficulty][player] = max(score, scores[game][player])
+
+    with open(scoreboard_path, "w") as scoreboard:
+        json.dump(scores, scoreboard)
